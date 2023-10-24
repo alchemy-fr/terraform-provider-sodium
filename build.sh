d2fb62f55d1eac19d0191b8c6b20d4c165adb64c
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-10-24 14:16:27 +0200

    WIP

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 
 rm -f ./src/terraform-provider-sodium
 
-#docker run -e GOCACHE=/usr/app/.cache --rm -ti -v ./src:/usr/app sodium-gobuilder bash
-docker run -e GOCACHE=/usr/app/.cache --rm -v ./src:/usr/app sodium-gobuilder go build -o terraform-provider-sodium
+#docker run -e GOCACHE=/usr/app/.cache --rm -ti -v .:/usr/app sodium-gobuilder bash
+docker run -e GOCACHE=/usr/app/.cache --rm -v .:/usr/app sodium-gobuilder go build -o terraform-provider-sodium
 
 target=~/.terraform.d/plugins/alchemy.fr/alchemy/sodium/1.0.0/linux_amd64
 mkdir -p $target
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,10 +2,8 @@ package provider
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -55,28 +53,4 @@ func (p *sodiumProvider) Resources(_ context.Context) []func() resource.Resource
 func (p *sodiumProvider) DataSources(_ context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
 	}
-}
-
-// toProvider can be used to cast a generic provider.Provider reference to this specific provider.
-// This is ideally used in DataSourceType.NewDataSource and ResourceType.NewResource calls.
-func toProvider(in any) (*sodiumProvider, diag.Diagnostics) {
-	if in == nil {
-		return nil, nil
-	}
-
-	var diags diag.Diagnostics
-
-	p, ok := in.(*sodiumProvider)
-
-	if !ok {
-		diags.AddError(
-			"Unexpected Provider Instance Type",
-			fmt.Sprintf("While creating the data source or resource, an unexpected provider type (%T) was received. "+
-				"This is always a bug in the provider code and should be reported to the provider developers.", in,
-			),
-		)
-		return nil, diags
-	}
-
-	return p, diags
 }
