07faccd98594feb68bbddc0fa3f3d988bc9461bf
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-10-24 11:37:37 +0200

    WIP

--- main.go
+++ main.go
@@ -6,29 +6,12 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/alchemy/terraform-provider-sodium/internal/provider"
-)
 
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
-
-var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary.
-	version string = "dev"
-
-	// goreleaser can pass other information to the main package, such as the specific commit
-	// https://goreleaser.com/cookbooks/using-main.version/
+	"github.com/alchemy/terraform-provider-sodium/provider"
 )
 
 func main() {
@@ -37,15 +20,13 @@ func main() {
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		Address: "registry.terraform.io/alchemy/sodium",
-		Debug:   debug,
-	}
-
-	err := providerserver.Serve(context.Background(), provider.New(version), opts)
-
+	err := providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
+		Address:         "registry.terraform.io/alchemy/sodium",
+		Debug:           debug,
+		ProtocolVersion: 5,
+	})
 	if err != nil {
-		log.Fatal(err.Error())
+		fmt.Printf("failed to initialize provider: %v\n", err)
+		os.Exit(1)
 	}
 }
